e760e707effddfde5401ae11ab312aa941e2df7e
johnnydoubt <cesar.tailleur@gmail.com>
2016-12-03 17:16:18 +0100

    ajout de la navbar simplonstrap, retrait des borders, script.js, mouseover effect avec couleurs simplon

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() { // Code relying on the DOM
+    const cards = document.getElementsByClassName("card");
+const reds = document.getElementsByClassName("redborder");
+const pinks = document.getElementsByClassName("pinkborder");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("mouseover", function() {
+          for(let i=0; i<cards.length; i++){
+            cards[i].classList.remove("blue");
+          }
+
+            this.classList.toggle("blue");
+        })
+    };
+
+    for (let i = 0; i < reds.length; i++) {
+        reds[i].addEventListener("mouseover", function() {
+
+            for(let i=0; i<cards.length; i++){
+              cards[i].classList.remove("red");
+            }
+            this.classList.toggle("red");
+
+        })
+    };
+    for (let i = 0; i < pinks.length; i++) {
+        pinks[i].addEventListener("mouseover", function() {
+
+            for(let i=0; i<cards.length; i++){
+              cards[i].classList.remove("pink");
+            }
+
+            this.classList.toggle("pink");
+
+        })
+    }
+});
