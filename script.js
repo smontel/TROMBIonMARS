1facc6319a6a5452223225430054fb6253da6e90
Chicken_Toast <montelsylvain@gmail.com>
2016-12-04 21:08:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() { // Code relying on the DOM
+
+    const cards = document.getElementsByClassName("card");
+    const whites = document.getElementsByClassName("whiteborder");
+    const pinks = document.getElementsByClassName("pinkborder");
+
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function() {
+            for (let i = 0; i < cards.length; i++) {
+                cards[i].classList.remove("selected");
+            }
+            this.classList.toggle("selected");
+        });
+    }
+
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("mouseover", function() {
+          for(let i=0; i<cards.length; i++){
+            cards[i].classList.remove("blue");
+          }
+
+            this.classList.toggle("blue");
+        })
+    };
+
+    for (let i = 0; i < whites.length; i++) {
+        whites[i].addEventListener("mouseover", function() {
+
+            for(let i=0; i<cards.length; i++){
+              whites[i].classList.remove("white");
+            }
+            this.classList.toggle("white");
+
+        })
+    };
+    for (let i = 0; i < pinks.length; i++) {
+        pinks[i].addEventListener("mouseover", function() {
+
+            for(let i=0; i<cards.length; i++){
+              pinks[i].classList.remove("pink");
+            }
+
+            this.classList.toggle("pink");
+
+        })
+    }
+});
